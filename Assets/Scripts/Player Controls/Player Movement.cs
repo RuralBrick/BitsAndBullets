3ff44f466e203df174ee7d6dcdf955e511f3aef2
Alex-Hen <Alexander.L.Henderson3@gmail.com>
2024-05-12 19:03:50 -0700

    Changed player prefab so that it can shoot bullets.  Added "gun barrel" collider and checks to stop from shooting bullets into wall

--- Assets/Scripts/Player Controls/Player Movement.cs
+++ Assets/Scripts/Player Controls/Player Movement.cs
@@ -1,3 +1,4 @@
+using StageDesignTestScripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -6,9 +7,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Declare Variables and constants
+    // Declare Prefabs that we use
+    [SerializeField] GameObject bulletPrefab;
+
+    
+
+
+    // Declare Variables and Constants
     [SerializeField] private float playerSpeed = 5; // Speed
     [SerializeField] private float coolDown = 3; // Reload timer
+
+
 
     // Declare Rigid body
     private Rigidbody2D rb;
@@ -18,11 +27,13 @@ public class PlayerMovement : MonoBehaviour
 
     // Decalre the Vector 2
     Vector2 move;
+    Vector2 orientation;
 
     // Declare the reloading counter
     private float reloadTimer;
-    
 
+    // Set bool for if in a wall
+    private bool inWall;
 
 
 
@@ -40,9 +51,14 @@ public class PlayerMovement : MonoBehaviour
 
         // Assign value to move vector - 0 for now
         move = new Vector2(0, 0);
+        orientation = new Vector2(0, 1);
 
         // Assign the reload timer to the default value - 0 - ready to fire
         reloadTimer = 0;
+
+        // CircleCollider2D bulletSpawn = GetComponent<CircleCollider2D>();
+        // Set the inWall Boolean
+        inWall = false;
     }
 
     // Update is called once per frame
@@ -56,10 +72,11 @@ public class PlayerMovement : MonoBehaviour
         // Rotate to put is in the direction of motion - ONLY if we are moving
         if (move != Vector2.zero)
         {
+            // Set the rotation
             rb.transform.rotation = Quaternion.Euler(Vector3.forward * Mathf.Atan2(-move.x, move.y) * Mathf.Rad2Deg);
-            // rb.AddTorque(5); // Other methods that didn't quite work
-            // rb.rotation = 1;
 
+            // Also store this orientation for firing purposes
+            orientation = move;
         }
         
         
@@ -82,10 +99,6 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRender.color = Color.green;
         }
-
-        print("The Reload Timer is: " + reloadTimer);
-
-
     }
 
 
@@ -101,20 +114,53 @@ public class PlayerMovement : MonoBehaviour
     // When we hit the fire key - no input, just a button push
     void OnFire()
     {
+        // If we are in a wall
+        if (inWall == true)
+        {
+            // Then do not fire
+            Debug.Log("In a wall - Can't Shoot");
+            return;
+        }
+
+
         // If we are still reloading
         if (reloadTimer > 0)
         {
             // Then do not fire
-            Debug.Log("Still reloading!");
+            Debug.Log("Still Reloading");
             return;
         }
 
         // Otherwise - fire - change color to red to show that we have to reload
         spriteRender.color = Color.red;
+
+        // Instantiate the bullet prefab
+        Instantiate(bulletPrefab, transform.position + (Vector3)orientation.normalized, rb.transform.rotation);
+
         Debug.Log("Firing");
 
         // Set reload timer
         reloadTimer = coolDown;
+    }
+
+
+
+    // Contact
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            inWall = true;
+        }
+    }
+
+    // Leave Contact
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            inWall = false;
+        }
     }
 
 
